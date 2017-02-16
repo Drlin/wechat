1fbed68330b9056909494de138068f00151da111
kzkxdzt <ljlbxy@126.com>
2017-02-17 00:00:35 +0800

    update

--- wechat-movie/app.js
+++ wechat-movie/app.js
@@ -2,6 +2,7 @@
 
 var koa = require('koa')
 var sha1 = require('sha1')
+var wechat = require('./wechat/g')
 
 
 var app = koa()
@@ -14,19 +15,7 @@ var config = {
   }
 }
 
-app.use(function *(next){
-  let query = this.query;
-  let token = config.wechat.Token;
-  let signature = query.signature;
-  let echostr = query.echostr;
-  let timestamp = query.timestamp;
-  let nonce = query.nonce;
-  let str = [token, timestamp, nonce].sort().join('');
-  let sha = sha1(str);
-  if (sha === signature) {
-    this.body = echostr + '';
-  }
-});
+app.use(wechat(config.wechat));
 
 app.listen(3000)
 console.log('成功启动服务，端口是 3000')
