dd61344c824edddf4f05595f8a3efa053680e3f4
kzkxdzt <ljlbxy@126.com>
2017-02-18 16:13:59 +0800

    格式化获取到的信息

--- wechat-movie/wechat/lib/Util.js
+++ wechat-movie/wechat/lib/Util.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const fs = require('fs');
-const Promise = require('bluebird');	
+const Promise = require('bluebird');
+const xml2js = require('xml2js');	
 
 module.exports = {
 	readFileAsync(fpath, encoding) {
@@ -23,5 +24,44 @@ module.exports = {
 				resolve()
 			})
 		})
+	},
+	parseXMLAsync(xml) {
+		return new Promise((resolve, reject) => {
+			xml2js.parseString(xml, {trim: true}, (err, content) => {
+				if (err) {
+					return reject(err)
+				} 
+				return resolve(content);
+			})
+		})
+	},
+	formatMessage(result) {
+		const message = {}
+  		if (typeof result === 'object') {
+    		let keys = Object.keys(result)
+		    for (let i = 0; i < keys.length; i++) {
+	        let item = result[keys[i]]
+	        let key = keys[i]
+		    if (!(item instanceof Array) || item.length === 0) {
+		        continue
+		    }
+		    if (item.length === 1) {
+		        let val = item[0]
+		        if (typeof val === 'object') {
+		          message[key] = formatMessage(val)
+		        }
+		        else {
+		          message[key] = (val || '').trim()
+		        }
+		      }
+		      else {
+		        message[key] = []
+		        for (let j = 0, k = item.length; j < k; j++) {
+		          message[key].push(formatMessage(item[j]))
+		        }
+		      }
+		    }
+		 }
+		return message
 	}
 }	
