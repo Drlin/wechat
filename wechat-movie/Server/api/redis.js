b6798fd229d2183dea0204d840ee7d6a8b5c9b61
kzkxdzt <ljlbxy@126.com>
2017-03-23 23:52:16 +0800

    update

--- wechat-movie/Server/api/redis.js
+++ wechat-movie/Server/api/redis.js
@@ -1,6 +1,6 @@
 const redisClient = require('../../wechat/config/redis')
 const REDIS_PREFIX = 'wechat_'
-console.log(redisClient)
+
 module.exports = {
 	setRedis: function *(key, value, expire) {
 		yield redisClient.set(`${REDIS_PREFIX}${key}`, JSON.stringify(value));
@@ -9,5 +9,13 @@ module.exports = {
 	getRedis: function *(key) {
 		const reply = yield redisClient.get(`${REDIS_PREFIX}${key}`);
 		return JSON.parse(reply)
-	}
+	},
+	saveToRank: function *(movie) {
+		const result = yield redisClient.zincrby('movie_rank', 1, movie);
+		return result;
+	},
+	getRank: function *() {
+		const result = yield redisClient.zrevrange('movie_rank', 0, -1);
+		return JSON.parse(result);
+	},
 }
