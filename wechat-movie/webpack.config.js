edd84315b01f422b24a20f393cc287acc94d4b6d
kzkxdzt <ljlbxy@126.com>
2017-04-03 09:25:23 +0800

    update

--- wechat-movie/webpack.config.js
+++ wechat-movie/webpack.config.js
@@ -1,7 +1,16 @@
+var path = require('path');
+
+
+var appPath = path.resolve('__dirname', 'wechat')
+
 module.exports = {
 	entry: './app.js',
 	output: {
 		path: './dist',
 		filename: 'js/[name].bundle.js'
+	},
+	module: {
+		//es6
+		loaders: []
 	}
 }
